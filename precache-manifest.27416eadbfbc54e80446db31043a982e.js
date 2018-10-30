931dbc0b96baea533b39949f0be055b3fb470d38
karkran <nikhil.karkra@anz.com>
2018-10-31 00:23:53 +1100

    Updates

---
+++ precache-manifest.27416eadbfbc54e80446db31043a982e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3f754b78c8b9c3b518b",
+    "url": "/react-hooks-todo/static/js/runtime~main.b3f754b7.js"
+  },
+  {
+    "revision": "7aa31f94bc8e4fcf0a5c",
+    "url": "/react-hooks-todo/static/js/main.7aa31f94.chunk.js"
+  },
+  {
+    "revision": "21d292598ba2f3852593",
+    "url": "/react-hooks-todo/static/js/1.21d29259.chunk.js"
+  },
+  {
+    "revision": "7aa31f94bc8e4fcf0a5c",
+    "url": "/react-hooks-todo/static/css/main.05513956.chunk.css"
+  },
+  {
+    "revision": "ca65325ec54376b7790f5eb0bdc2a4a9",
+    "url": "/react-hooks-todo/index.html"
+  }
+];
